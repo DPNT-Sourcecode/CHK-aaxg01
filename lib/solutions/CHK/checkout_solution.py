3fc96b0b877ed0f9aa3d978b9baee93442c47d4d
sbx_user1051 <sbx_user1051@169.254.34.193>
2024-05-27 22:12:36 +0000

    Mon May 27 22:12:36 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,5 +1,36 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
+
+sku_dict = {
+    'A': 50,
+    'B': 30,
+    'C': 20,
+    'D': 15,
+    'E': 40,
+    'F': 10,
+    'G': 20,
+    'H': 10,
+    'I': 35,
+    'J': 60,
+    'K': 80,
+    'L': 90,
+    'M': 15,
+    'N': 40,
+    'O': 10,
+    'P': 50,
+    'Q': 30,
+    'R': 50,
+    'S': 30,
+    'T': 20,
+    'U': 40,
+    'V': 50,
+    'W': 20,
+    'X': 90,
+    'Y': 10,
+    'Z': 50,
+}
+
+
 def checkout(skus: str) -> int:
     if not skus:
         return 0
@@ -11,8 +42,10 @@ def checkout(skus: str) -> int:
     d_count = 0
     e_count = 0
     f_count = 0
+    g_count = 0
+    h_count = 0
 
-    if not set(skus) <= {'A', 'B', 'C', 'D', 'E', 'F'}:
+    if not set(skus) <= set(sku_dict.keys()):
         return -1
 
     if 'A' in skus:
@@ -30,7 +63,6 @@ def checkout(skus: str) -> int:
     if 'E' in skus:
         e_count += skus.count('E')
         e_multiple = e_count // 2
-        e_remainder = e_count % 2
 
         if b_count:
             b_count += (-1 * e_multiple)
@@ -38,12 +70,13 @@ def checkout(skus: str) -> int:
     if 'F' in skus:
         f_count += skus.count('F')
         f_multiple = f_count // 3
-        f_remainder = f_count % 3
 
         f_count += (-1 * f_multiple)
 
-    a_multiple = a_count // 3
-    a_remainder = a_count % 3
+    if 'G' in skus:
+        g_count += skus.count('G')
+    if 'H' in skus:
+        h_count += skus.count('H')
 
     if a_count // 5:
         a_multiple = a_count // 5
@@ -57,15 +90,29 @@ def checkout(skus: str) -> int:
         total += (a_multiple * 130)
         a_count = a_remainder
 
-    total += a_count * 50
+    if h_count // 10:
+        h_multiple = h_count // 10
+        h_remainder = h_count % 10
+        total += (h_multiple * 80)
+        h_count = h_remainder
+
+    if h_count // 5:
+        h_multiple = h_count // 5
+        h_remainder = h_count % 5
+        total += (h_multiple * 45)
+        h_count = h_remainder
 
     b_multiple = b_count // 2
     b_remainder = b_count % 2
 
-    total += (b_multiple * 45) + (b_remainder * 30)
-    total += 20 * c_count
-    total += 15 * d_count
-    total += 40 * e_count
-    total += 10 * f_count
+    total += sku_dict['A'] * a_count
+    total += (b_multiple * 45) + (b_remainder * sku_dict['B'])
+    total += sku_dict['C'] * c_count
+    total += sku_dict['D'] * d_count
+    total += sku_dict['E'] * e_count
+    total += sku_dict['F'] * f_count
+    total += sku_dict['G'] * g_count
+    total += sku_dict['H'] * h_count
 
     return total
+
