673ea857bf4cc1d3ae3645ec22e1f6fb954e0820
sbx_user1051 <sbx_user1051@169.254.34.193>
2024-05-27 21:00:58 +0000

    Mon May 27 21:00:58 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,15 +1,25 @@
-
-
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus: str) -> int:
     skus_lower = skus.lower()
     total = 0
+    a_count = 0
+    b_count = 0
+    c_count = 0
+    d_count = 0
     if 'a' in skus_lower:
         a_count = skus_lower.count('a')
+        multiple = a_count // 3
+        remainder = a_count % 3
+
+        total += (multiple * 130) + (remainder * 50)
 
     if 'b' in skus_lower:
         b_count = skus_lower.count('b')
+        multiple = b_count // 2
+        remainder = b_count % 2
+
+        total += (multiple * 45) + (remainder * 30)
 
     if 'c' in skus_lower:
         c_count = skus_lower.count('c')
@@ -22,5 +32,6 @@ def checkout(skus: str) -> int:
     if not a_count or not b_count or not c_count or not d_count:
         return -1
 
+    return total
 
 
