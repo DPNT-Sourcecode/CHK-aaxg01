dc18333cbdc2a17dc45d0538b5cb951143295970
sbx_user1051 <sbx_user1051@169.254.34.193>
2024-05-27 20:57:58 +0000

    Mon May 27 20:57:58 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,5 +2,25 @@
 
 # noinspection PyUnusedLocal
 # skus = unicode string
-def checkout(skus):
-    raise NotImplementedError()
+def checkout(skus: str) -> int:
+    skus_lower = skus.lower()
+    total = 0
+    if 'a' in skus_lower:
+        a_count = skus_lower.count('a')
+
+    if 'b' in skus_lower:
+        b_count = skus_lower.count('b')
+
+    if 'c' in skus_lower:
+        c_count = skus_lower.count('c')
+        total += 20 * c_count
+
+    if 'd' in skus_lower:
+        d_count = skus_lower.count('d')
+        total += 15 * d_count
+
+    if not a_count or not b_count or not c_count or not d_count:
+        return -1
+
+
+
