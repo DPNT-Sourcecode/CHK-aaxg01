97311f29f8d0475b3a996c8ed26dd8fe63e11ca7
sbx_user1051 <sbx_user1051@169.254.34.193>
2024-05-27 21:04:10 +0000

    Mon May 27 21:04:10 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,38 +4,38 @@ def checkout(skus: str) -> int:
     if not skus:
         return 0
 
-    skus_lower = skus.lower()
     total = 0
     a_count = 0
     b_count = 0
     c_count = 0
     d_count = 0
-    if 'a' in skus_lower:
-        a_count = skus_lower.count('a')
+    if 'A' in skus:
+        a_count = skus.count('A')
         multiple = a_count // 3
         remainder = a_count % 3
 
         total += (multiple * 130) + (remainder * 50)
 
-    if 'b' in skus_lower:
-        b_count = skus_lower.count('b')
+    if 'B' in skus:
+        b_count = skus.count('B')
         multiple = b_count // 2
         remainder = b_count % 2
 
         total += (multiple * 45) + (remainder * 30)
 
-    if 'c' in skus_lower:
-        c_count = skus_lower.count('c')
+    if 'C' in skus:
+        c_count = skus.count('C')
         total += 20 * c_count
 
-    if 'd' in skus_lower:
-        d_count = skus_lower.count('d')
+    if 'D' in skus:
+        d_count = skus.count('D')
         total += 15 * d_count
 
     if not a_count and not b_count and not c_count and not d_count:
         return -1
 
     return total
+
 
 
 
